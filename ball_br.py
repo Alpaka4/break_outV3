f3d6eef196485869b9196863952c23fa2e3292ad
Germankhl <113830662+Alpaka4@users.noreply.github.com>
2023-02-05 13:20:44 +0300

    Add files via upload

---
+++ ball_br.py
@@ -0,0 +1,34 @@
+from config_2 import *
+class Ball:
+    def __init__(self):
+        self.r = 20
+        self.x = SCREEN_WIDTH//2
+        self.y = SCREEN_HEIGHT // 2
+        self.speed_x=7
+        self.speed_y=6
+        self.left_score = 0
+    def update(self):
+        # передвигаем мяч по экарну
+        self.x+=self.speed_x
+        self.y+=self.speed_y
+        #выход за левый кран экрана
+        if self.x <= self.r:
+            # летел налево - полетел направо
+            self.speed_x=-self.speed_x
+            self.speed_x-=2
+            self.speed_y-=2
+        if self.x>= SCREEN_WIDTH - self.r:
+            #летел направо - полетел налево
+            self.speed_x= -self.speed_x
+            self.speed_x-=2
+            self.speed_y-=2
+            #выход за верхний край
+        if self.y<=self.r:
+            #летел наверх-полетел вниз
+            self.speed_y=-self.speed_y
+            #выход за нижний край
+        if self.y>=SCREEN_HEIGHT - self.r:
+            #летел вниз-полетел наверх
+            self.speed_y=-self.speed_y
+            self.x=SCREEN_WIDTH//2
+            self.y = SCREEN_HEIGHT // 2
